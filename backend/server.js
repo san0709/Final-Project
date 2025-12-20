15e3522f82e24504fa22503d546f81f26da7673a
san0709 <sandhiya6797@gmail.com>
2025-12-20 23:26:11 +0530

    save

--- backend/server.js
+++ backend/server.js
@@ -83,7 +83,7 @@ app.use(errorHandler);
 
 /* -------------------- SERVER -------------------- */
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
