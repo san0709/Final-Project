c05f844586d21c3908fa6eabb4e351d7cc1b1e88
san0709 <sandhiya6797@gmail.com>
2025-12-22 16:54:01 +0530

    save

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ const app = express();
 
 const allowedOrigins = [
     'http://localhost:5173',
-    'http://localhost:3000',
+    'http://localhost:4000',
     'https://final-project-alpha-neon-27.vercel.app',
 ];
 
